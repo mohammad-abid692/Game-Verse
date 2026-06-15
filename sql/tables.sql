1061e3b90786b8d7b57ff50f996d7b41915d62e8
mohammad-abid692 <md.abidansari692@gmail.com>
2026-06-15 12:14:12 +0530

    Created games, developers and publishers tables and made a connection between them.

--- sql/tables.sql
+++ sql/tables.sql
@@ -8,3 +8,28 @@ CREATE TABLE users (
     join_date DATE DEFAULT CURRENT_DATE,
     coin_balance INTEGER NOT NULL DEFAULT 0
 );
+
+CREATE TABLE games (
+    game_id SERIAL PRIMARY KEY,
+    title VARCHAR(200) NOT NULL UNIQUE,
+    price DECIMAL(6,2) NOT NULL,
+    release_date DATE DEFAULT CURRENT_DATE,
+    developer_id INTEGER NOT NULL,
+    publisher_id INTEGER NOT NULL,
+    FOREIGN KEY(developer_id)
+        REFERENCES developers(developer_id),
+    FOREIGN KEY(publisher_id)
+        REFERENCES publishers(publisher_id) 
+);
+
+CREATE TABLE developers(
+    developer_id SERIAL PRIMARY KEY,
+    developer_name VARCHAR(200) NOT NULL,
+    country VARCHAR(80) NOT NULL
+);
+
+CREATE TABLE publishers(
+    publisher_id SERIAL PRIMARY KEY,
+    publisher_name VARCHAR(200) NOT NULL,
+    country VARCHAR(80) NOT NULL
+);
